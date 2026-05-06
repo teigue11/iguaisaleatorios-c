5485d2d981e2dd6cadd9c5e53bd488894974b945
teigue11 <dericknjd@gmail.com>
2026-05-05 22:33:54 -0300

    Update function_numero_aleatorio.c

--- function_numero_aleatorio.c
+++ function_numero_aleatorio.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include "function_numero_aleatorio.c"
 
-void main()
+int main()
 {
     int numero = numero_aleatorio();
     assert(numero > 0);
     printf("Teste executado com sucesso");
+
+    return 0;
 }
