bb2928b1793668e70c3fbea541e0e1407810cf0a
teigue11 <dericknjd@gmail.com>
2026-05-05 22:29:01 -0300

    Update test_iguaisaleatorios.c

---
+++ test_iguaisaleatorios.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <stdio.h>
+#include "function_numero_aleatorio.c"
+
+int main()
+{
+    int numero = numero_aleatorio();
+    assert(numero > 0);
+    printf("Teste executado com sucesso");
+
+  retun 0;
+}
